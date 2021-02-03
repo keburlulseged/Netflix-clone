cf95ca547aa8746d0e5047f2d14fb4a4633f1d22
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-02-03 13:05:36 -0800

    finish styling banner and link to TMDB database

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,31 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Banner.css";
+import axios from "../axios";
+import requests from "../Requests";
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
 
   return (
     <header
-      class="banner"
+      className="banner"
       style={{
-        backgroundImage: `url('https://static.fabrik.io/y48/f66ad9d41a69dfff.jpeg?auto=format&lossless=1&w=2880&h=5120&fit=max&s=f32527706fed3b78853e114fbb1892e0')`,
+        backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
         backgroundSize: "cover",
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Lupin</h1>
+        <h1 className="banner__title">{movie?.name || movie?.original_name}</h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <div className="banner__discription">
-          {truncate(
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Estlaudantium facere repellendus saepe culpa quaerat fugit? Voluptatemrepudiandae sed, illo eveniet, quas ut sint culpa enim quisperspiciatis, ratione nemo?",
-            150
-          )}
+          {truncate(movie?.overview, 150)}
         </div>
       </div>
       <div className="banner--fadeBottom" />
