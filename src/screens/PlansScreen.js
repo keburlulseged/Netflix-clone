3bdacb16338e4b13987651bed0325da2233f08ed
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-02-06 22:58:57 -0800

    hook up payment processing logic and checkout with stripe

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,10 +1,30 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { loadStripe } from "@stripe/stripe-js";
 import db from "../firebase";
 import "./PlansScreen.css";
 
 function PlansScreen() {
   const [products, setProducts] = useState([]);
+  const user = useSelector(selectUser);
+  const [subscription, setSubscription] = useState(null);
 
+  useEffect(() => {
+    db.collection("customers")
+      .doc(user.uid)
+      .collection("subscriptions")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach(async (subscription) => {
+          setSubscription({
+            role: subscription.data().role,
+            current_period_end: subscription.data().current_period_end.seconds,
+            current_period_start: subscription.data().current_period_start,
+          });
+        });
+      });
+  }, [user.id]);
   useEffect(() => {
     db.collection("products")
       .where("active", "==", true)
@@ -26,19 +46,75 @@ function PlansScreen() {
   }, []);
 
   console.log(products);
+  console.log(subscription);
+
+  const loadCheckout = async (priceId) => {
+    const docRef = await db
+      .collection("customers")
+      .doc(user.uid)
+      .collection("checkout_sessions")
+      .add({
+        price: priceId,
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      });
+
+    docRef.onSnapshot(async (snap) => {
+      const { error, sessionId } = snap.data();
+
+      if (error) {
+        // Show an error to your customer and
+        // Inspect your Cloud Function logs in the Firebase console
+        alert(`An error eccored: ${error.message}`);
+      }
+
+      if (sessionId) {
+        // We have a session, let's redirect to Checkout
+        // Init Stripe
+
+        const stripe = await loadStripe(
+          "pk_test_51HzHI5H9oQuu69qkb5N1EJ0GMIQy4s9ZLYnwpOMJP1Hdib5Wv5fGRZkeVvYjj8jk8h25sryExsYfRbP4qzG6LxjI00QCupFDeP"
+        );
+        stripe.redirectToCheckout({ sessionId });
+      }
+    });
+  };
 
   return (
     <div className="plansScreen">
+      <br />
+      {subscription && (
+        <p>
+          Renewal date:{" "}
+          {new Date(
+            subscription?.current_period_end * 1000
+          ).toLocaleDateString()}
+        </p>
+      )}
       {Object.entries(products).map(([productId, productData]) => {
         // TODO: add logic to check if user subscription is actiove
+        const isCurrentPackage = productData.name
+          ?.toLowerCase()
+          .includes(subscription?.role);
         return (
-          <div className="plansScreen__plan">
+          <div
+            key={productId}
+            className={`${
+              isCurrentPackage && "plansScreen__plan--disabled"
+            } plansScreen__plan`}
+          >
             <div className="plansScreen__info">
               <h5>{productData.name}</h5>
               <h6>{productData.description}</h6>
             </div>
 
-            <button>Subscribe</button>
+            <button
+              onClick={() =>
+                !isCurrentPackage && loadCheckout(productData.prices.priceId)
+              }
+            >
+              {isCurrentPackage ? "Current Plan" : "Subscribe"}
+            </button>
           </div>
         );
       })}
