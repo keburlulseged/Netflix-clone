bc56d8a860a84d4171a746cd54c41c4667de5126
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-02-06 20:08:23 -0800

    use useEffect to grab products from firestoreDB and add to store

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
 import { auth } from "../firebase";
 import Nav from "../components/Nav";
+import PlansScreen from "./PlansScreen";
 import "./ProfileScreen.css";
 
 function ProfileScreen() {
@@ -21,7 +22,7 @@ function ProfileScreen() {
             <h2>{user.email}</h2>
             <div className="profileScreen__plans">
               <h3>Plans</h3>
-              <p></p>
+              <PlansScreen />
               <button
                 onClick={() => auth.signOut()}
                 className="profileScreen__signOut"
